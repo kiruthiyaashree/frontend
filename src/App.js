78b666cd5f006820bc4c82cd6e88117dfa9866ab
KIRUTHIYAASHREE S P <98012434+kiruthiyaashree@users.noreply.github.com>
2024-01-28 00:00:56 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// frontend/src/App.js
+
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleChangeName = (e) => {
+        setName(e.target.value);
+    };
+
+    const handleChangeEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handleOnSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('/submit-form', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ name, email })
+            });
+
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        <>
+            <h1>This is React WebApp </h1>
+            <form action="/submit-form" method='post' onSubmit={handleOnSubmit}>
+                <input type="text" placeholder="name"
+                    value={name} onChange={handleChangeName} />
+                <input type="email" placeholder="email"
+                    value={email} onChange={handleChangeEmail} />
+                <button type="submit">submit</button>
+            </form>
+        </>
+    );
 }
 
 export default App;
